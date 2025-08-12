ffc3657906ccc11a6d07abcfdec2c3cf87bb406b
Juan Correa <cjuancorreac2004@outlook.com>
2025-08-11 21:55:40 -0500

    chat implementado correctamente

--- chat.js
+++ chat.js
@@ -1,3 +1,20 @@
 const chatinput= document.querySelector('.chat-input')
 const chatMessages=document.querySelector('.chat-messages')
 const chatInputForm= document.querySelector('.chat-input-form')
+
+const chatMessageElement = (message) =>`
+  <div class="message ${'gray-bg'}">
+    <div class="message-text">${message.text}</div>
+  </div>
+`
+const sendMessage = (e) => {
+    e.preventDefault()
+    const message={text: chatinput.value}
+    chatMessages.innerHTML += chatMessageElement(message)
+    chatinput.value = '' 
+    chatMessages.scrollTop = chatMessages.scrollHeight // Desplazar hacia abajo
+    
+}
+
+chatInputForm.addEventListener('submit', sendMessage)
+
